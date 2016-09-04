f88c647948612a26e95b5e72fef911dd717925de
JuanKman94 <juan.carlos.menonita@gmail.com>
2016-09-04 02:51:13 -0500

    Add bash script to take a screenshot * Depends on X11 and ImageMagick

---
+++ bin/screenshot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUT_DIR=$HOME/pictures/screenshots
+
+mkdir $OUT_DIR
+
+if type xwd >/dev/null 2>&1; then
+    if type convert >/dev/null 2>&1; then
+        xwd -root | convert - "$OUT_DIR/$(date --iso-8601=minutes).png"
+    fi
+fi
