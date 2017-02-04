12cd1bd4df870fcf40ba0e992a495baa6bb4f307
JuanKman94 <juan.carlos.menonita@gmail.com>
2017-02-04 15:59:21 -0600

    Update syntax for install-terminus-font script

--- install-terminus-font.sh
+++ install-terminus-font.sh
@@ -1,6 +1,9 @@
-PREV_DIR = `pwd`
+#!/bin/sh
+
+PREV_DIR=$(pwd)
 cd /tmp
 
+######### TERMINUS #########
 wget http://downloads.sourceforge.net/project/terminus-font/terminus-font-4.40/terminus-font-4.40.tar.gz
 tar zxvf terminus-font-4.40.tar.gz
 cd terminus-font-4.40
@@ -12,5 +15,5 @@ sudo make install-pcf
 sudo make fontdir
 make clean
 
-cd $PREV_DIR
 rm -r /tmp/terminus-font-4.40
+cd $PREV_DIR
